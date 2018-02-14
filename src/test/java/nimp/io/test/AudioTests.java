2635fada182235ee05cab77ba2d048169a89b8cb
lduverge <Cadreras123>
2018-02-14 10:11:03 +0100

    first test

---
+++ src/test/java/nimp/io/test/AudioTests.java
@@ -0,0 +1,22 @@
+package nimp.io.test;
+
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import javax.validation.constraints.AssertTrue;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(JMockit.class)
+public class AudioTests {
+
+    @Test
+    public void cutAudio()
+    {
+        assertTrue(false);
+    }
+
+
+}
